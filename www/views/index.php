a0b3d58a1e85f3ffb8688b9d0696e6f9c40821da
Oksana Semenuk <ksuha_077@mail.ru>
2015-12-01 14:46:24 +0200

    VIEWS- внешний вид(это видит пользователь).

--- www/views/index.php
+++ www/views/index.php
@@ -1,7 +1,35 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Пользователь
- * Date: 01.12.2015
- * Time: 14:01
- */
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<center>
+    <table>
+        <tr>
+            <th>Дата</th>
+            <th>Название</th>
+            <th>Текст новости</th>
+        </tr>
+        <tr>
+            <th><BR></th>
+        </tr>
+        <?php foreach ($items as $item): ?>
+            <tr>
+                <td><?php echo $item['dt']; ?></td>
+                <td><?php echo $item['news']; ?></td>
+                <td><a href="show_news.php/?id=<?php echo $item['id']; ?>">читать новость
+                        полностью >>>>> </a></td>
+            </tr>
+        <?php endforeach; ?>
+        </tr>
+    </table>
+</center>
+<br /><br />
+<a href="./add.php">Добавить новость</a>
+<br /><br />
+<a href="./delete.php">Удалить новость</a>
+<br /><br />
+<a href="./update.php">Редактировать новость</a>
+</body>
+</html>
