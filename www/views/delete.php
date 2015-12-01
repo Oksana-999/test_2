7135da7a6290f6342580adb89c47107f65d92756
Oksana Semenuk <ksuha_077@mail.ru>
2015-12-01 14:57:51 +0200

    VIEWS- внешний вид.

--- www/views/delete.php
+++ www/views/delete.php
@@ -1,7 +1,15 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Пользователь
- * Date: 01.12.2015
- * Time: 14:02
- */
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Новостная лента</title>
+</head>
+<body>
+<form method="post">
+    Необходимо указать название новости:
+    <input type="text" name="delete_news" title="Напиште название новости, которую необходимо удалить"/>
+    <br /><br />
+    <input type="submit" value="Удалить новость" /><br />
+</form>
+</body>
+</html>
