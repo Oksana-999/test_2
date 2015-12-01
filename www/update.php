0baa42fca36238201d0ec91749516359f9ff7903
Oksana Semenuk <ksuha_077@mail.ru>
2015-12-01 14:41:41 +0200

    КОНТРОЛЛЕР формы. Отвечает за изменение новости.

--- www/update.php
+++ www/update.php
@@ -1,7 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Пользователь
- * Date: 01.12.2015
- * Time: 14:01
- */
+//КОНТРОЛЛЕР формы,
+require __DIR__ . '/models/news.php';
+
+//бизнес- логика
+if (!empty($_POST)) {
+
+    $data_update = array();
+    if (!empty($_POST['news_update'])) {
+        $data_update['news'] = $_POST['news_update'];
+
+    }
+
+    if (!empty($_POST['text_update'])) {
+        $data_update['text'] = $_POST['text_update'];
+
+    }
+    if (isset($data_update['news']) && isset($data_update['text'])) {
+        News_update( $data_update );
+        header('Location: index.php');
+        die;
+    }
+}
+include __DIR__ . '/views/update.php';
+?>
