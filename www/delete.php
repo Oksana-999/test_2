e768c6c553edfb6f167e27edd036c7f4bd66cde8
Oksana Semenuk <ksuha_077@mail.ru>
2015-12-01 14:43:29 +0200

    КОНТРОЛЛЕР формы. Отвечает за удаление новости.

--- www/delete.php
+++ www/delete.php
@@ -1,7 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Пользователь
- * Date: 01.12.2015
- * Time: 14:00
- */
+require_once __DIR__. "/models/news.php";
+if (!empty($_POST)) {
+
+    $data_delete = array();
+    if (!empty($_POST['delete_news'])) {   //1 час 27 Geekbrains урок 8
+        $data_delete['news'] = $_POST['delete_news'];
+
+    }
+    if (isset($data_delete['news'])) {
+        News_delete($data_delete);
+        header('Location: index.php');
+        die;
+    }
+}
+
+include __DIR__ . '/views/delete.php';
