22f0f2a04a164846b75cc4118477dd2ddf694f6d
Oksana Semenuk <ksuha_077@mail.ru>
2015-12-01 14:44:02 +0200

    КОНТРОЛЛЕР формы. Отвечает за добавление новости.

--- www/add.php
+++ www/add.php
@@ -1,7 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Пользователь
- * Date: 01.12.2015
- * Time: 14:00
- */
+//КОНТРОЛЛЕР формы
+require __DIR__ . '/models/news.php';
+if (!empty($_POST)) {
+
+    $data = array();
+    if (!empty($_POST['news'])) {
+        $data['news'] = $_POST['news'];
+
+    }
+
+    if (!empty($_POST['text'])) {
+        $data['text'] = $_POST['text'];
+
+    }
+
+    if (isset($data['news']) && isset($data['text'])) {
+        News_insert($data);
+        header('Location: index.php');
+        die;
+    }
+}
+
+include __DIR__ . '/views/add.php';
+?>
