0920057439390a9bdea0b31924586066b4f05969
Oksana Semenuk <ksuha_077@mail.ru>
2015-12-01 14:56:33 +0200

    VIEWS- внешний вид.

--- www/views/update.php
+++ www/views/update.php
@@ -1,7 +1,16 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Пользователь
- * Date: 01.12.2015
- * Time: 14:03
- */
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Новостная лента</title>
+</head>
+<body>
+<form method="post">
+   <label>Название новости, которую необходимо редактировать:</label><br/>
+    <input type="text" name="news_update" title="Напиште название новости, которую необходимо редактировать"/><br /><br />
+    <label>Новый текст новости:</label><br/>
+    <input type="text" name="text_update" title="Напиште новый текст новости"/><br /><br />
+    <input type="submit" value="Редактировать новость" /><br />
+</form>
+</body>
+</html>
