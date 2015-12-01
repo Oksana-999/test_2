e1644c32eb9c010b2296eb08d88b440934bfb54d
Oksana Semenuk <ksuha_077@mail.ru>
2015-12-01 15:00:32 +0200

    Модель

--- www/models/news.php
+++ www/models/news.php
@@ -1,7 +1,39 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Пользователь
- * Date: 01.12.2015
- * Time: 14:03
- */
+require_once __DIR__ . '/../files/DB.php';
+
+function getAll()
+{
+    $db = new DB ();
+    $sql = 'SELECT * FROM messages ORDER BY dt DESC';
+    return $db->getAlldb($sql);
+}
+
+function News_insert($data)
+{
+    $db = new DB ();
+    $sql = "INSERT INTO messages (dt, news, text) VALUES
+	('" . date('Y-m-d H:i:s') . "', '" . $data['news'] . "', '" . $data['text'] . "')";
+    return $db->insert($sql);
+}
+
+function Selection_id($get)
+{
+    $db  =  new  DB ();
+    $sql = "SELECT id, text FROM messages WHERE id='" . $get . "'";
+    return $db->onlyChosen($sql);
+
+}
+
+function News_delete($data_delete)
+{
+    $db  =  new  DB ();
+    $sql = "DELETE FROM messages WHERE  news='" . $data_delete['news'] . "'";
+    return $db->delete($sql);
+}
+
+function News_update($data_update)
+{
+    $db  =  new  DB ();
+    $sql = "UPDATE messages SET text='" . $data_update['text'] . "'  WHERE news='" . $data_update['news'] .  "'";
+    return $db->update($sql);
+}
